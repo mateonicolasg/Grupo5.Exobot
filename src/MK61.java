5dc69bc4e8dda6675c0036c7b72648976cde5b46
mateonicolasg <mateo.simbana@epn.edu.ec>
2024-01-26 17:37:29 -0500

    creando las clases

---
+++ src/MK61.java
@@ -0,0 +1,3 @@
+public class MK61 implements IDispara {
+
+}
---
+++ src/Mecatronico.java
@@ -0,0 +1,12 @@
+public class Mecatronico {
+
+
+    public void armarExobot(String instrucciones){
+        System.out.println("armando exobot");
+    }
+
+    public void personalizar(String habilidad){
+        System.out.println("personalizando ");
+    }
+
+}
