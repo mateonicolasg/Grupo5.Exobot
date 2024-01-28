9b3eea734e116a41595b8c00b373d8f2af84346a
EdisonV5329 <edison.vera02@epn.edu.ec>
2024-01-27 20:08:41 -0500

    actualizando  Mecatronico, IAbot, Exobot y Soldado

--- src/Iabot.java
+++ src/Iabot.java
@@ -1,3 +1,34 @@
-public class Iabot {
+import java.util.ArrayList;
+
+public class IAbot {
+    private ArrayList<String> lstCodigoAccion;
+    private ArrayList<String> lstCodigoUnico;
+
+    // constructor
+    public IAbot() {
+        lstCodigoAccion = new ArrayList<>();
+        lstCodigoUnico = new ArrayList<>();
+    }
+
+    // metodos
+
+    public void leerCodigoUnico(){}
+                                    
+    public String otrogarCodigoAccion(){
+        String codigo="ssdkvjd";
+
+        return codigo;
+    }
+    
+    public void implementarIdioma() {
+
+    }
+
+    public String otorgarCodigoUnico(){
+        String codigo="ssdkvjd";
+
+        return codigo;
+    }
+
 
 }
