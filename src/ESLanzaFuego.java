4b401230fa000fb488a392bd3da6e7a54644261e
Estefano2001 <151555121+Estefano2001@users.noreply.github.com>
2024-01-29 22:04:17 -0500

    Update and rename SMLanzaFuego.java to ESLanzaFuego.java

--- src/ESLanzaFuego.java
+++ src/ESLanzaFuego.java
@@ -1,11 +1,11 @@
-public class SMLanzaFuego implements FTIDispara, SMIArma {
+public class ESLanzaFuego implements FTIDispara, ESIArma {
     @Override
     public void ftDisparar() {
         System.out.println("Lanzando fuego");
     }
 
     @Override
-    public void smObtenerEnergia(SAFuentePoder smFuentePoder) {
+    public void esObtenerEnergia(SAFuentePoder smFuentePoder) {
         System.out.println("Lanza fuego obteniendo energia y con potencia " + smFuentePoder.smGetSmNivelEnergia());
     }
 }
